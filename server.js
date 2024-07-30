2cf24cf1b35b07caff7f87c7b409c279e37f8aa5
misdarmanto <misdarmanto1922@gmail.com>
2024-07-30 13:59:34 +0700

    _

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express()
 const PORT = 3000
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(PORT, () => {
